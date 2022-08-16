d6fe80ab4e290084942634ad59d706d2f9c91804
Matheus Maculan <matheus.maculan@hotmail.com>
2022-08-16 00:00:53 +0000

    CRUD finalized

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 
 export const app = express();
 
+app.use(express.json());
+
 const livros = [
   {
     id: 1,
@@ -20,3 +22,33 @@ app.get("/", (req, res) => {
 app.get("/livros", (req, res) => {
   res.status(200).json(livros);
 });
+
+app.get("/livros/:id", (req, res) => {
+  const { id } = req.body;
+  const livro = buscaLivro(parseInt(req.params.id));
+  res.json(livros[livro]);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("Livro criado com sucesso");
+});
+
+app.put("/livros/:id", (req, res) => {
+  const { id } = req.body;
+  const livro = buscaLivro(parseInt(req.params.id));
+  livros[livro].titulo = req.body.titulo;
+  res.status(200).send("Livro atualizado com sucesso");
+  res.json(livros[livro]);
+});
+
+app.delete("/livros/:id", (req, res) => {
+  const { id } = req.body;
+  const livro = buscaLivro(parseInt(req.params.id));
+  livros.splice(livro, 1);
+  res.status(200).send(`Livro ${id} removido com sucesso`);
+});
+
+function buscaLivro(id: number) {
+  return livros.findIndex((livro) => livro.id == id);
+}
